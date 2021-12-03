bf7f850a45856bdb2a14564c39a76385f82951e9
rcardenass <rcardenass@hotmail.com>
2021-12-03 18:48:55 -0500

    avance

--- mini-erp/src/pages/privado/peticionoferta/components/Filamaterial/index.jsx
+++ mini-erp/src/pages/privado/peticionoferta/components/Filamaterial/index.jsx
@@ -1,26 +1,43 @@
-// import axios from "axios";
+import axios from "axios";
 // import React from "react";
+import { useEffect, useState } from "react";
 import "./filamaterial.css";
 
 function Filamaterial(){
-    const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+    const [materiales, setMateriales]  = useState([]);
+    // const [tableMateriales, settableMAteriales] = useState([]);
 
+    const peticionGet = async() => {
+        await axios.get("link.................")
+        .then(response => {
+            setMateriales(response.data);
+            settableMAteriales(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    useEffect(() => {
+        peticionGet();
+    },[]);
 
     return(
         <>
-            <tr>
-                <th style={{width: '20px'}}>
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      id="select"
-                    />
-                </th>
-                <th scope="row">0000000001</th>
-                <td>Mouse</td>
-                <td>Tecnologia</td>
-             </tr>
-  
+            {materiales &&
+             materiales.map((material) => (
+                <tr key={material.id}>
+                     <th style={{width: '20px'}}>
+                        <input
+                          class="form-check-input"
+                          type="checkbox"
+                          id="select"
+                        />
+                    </th>
+                    <th scope="row">{material.sku}</th>
+                    <td>{material.name}</td>
+                    <td>{material.grpart}</td>
+                </tr>
+                ))}  
         </>
     );
 }
